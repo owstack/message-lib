72dbde463a4949c84469638c6e69e72cc0b4465c
ajp8164 <andyphillipson@gmail.com>
2019-02-27 08:35:06 -0500

    Add some tests

--- test/message.js
+++ test/message.js
@@ -16,6 +16,7 @@ describe('Message', function() {
   var address = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
   var badAddress = 'mmRcrB5fTwgxaFJmVLNtaG8SV454y1E3kC';
   var privateKey = keyLib.PrivateKey.fromWIF('cPBn5A4ikZvBTQ8D7NnvHZYCAxzDZ5Z2TSGW2LkyPiLxqYaJPBW4', 'testnet');
+
   var text = 'hello, world';
   var signatureString = 'H/DIn8uA1scAuKLlCx+/9LnAcJtwQQ0PmcPrJUq90aboLv3fH5fFvY+vmbfOSFEtGarznYli6ShPr9RXwY9UrIY=';
 
@@ -25,6 +26,16 @@ describe('Message', function() {
   var badSignature = Signature.fromCompact(new Buffer(badSignatureString, 'base64'));
 
   var publicKey = privateKey.toPublicKey();
+
+  var bchPrivateKey = new keyLib.PrivateKey('KwF9LjRraetZuEjR8VqEq539z137LW5anYDUnVK11vM3mNMHTWb4', 'bch');
+  var bchPublicKey = bchPrivateKey.toPublicKey();
+  var bchSignatureString = 'IOMkHmNmz7lCnIeuRMvRsq1G4tDN4gJkGxzlm9bek1dYU2ZlLsGd6/9yF3Gr0YVVDAGjK+OuXCoMUBOqXMQQ6eA=';
+  var bchSignature = Signature.fromCompact(new Buffer(bchSignatureString, 'base64'));
+
+  var ltcPrivateKey = new keyLib.PrivateKey('T35QnUj2z2sAg5NHg8n73RaXvrgRQb6Ubk7jeHwYatXDHFvTPJyA', 'ltc');
+  var ltcPublicKey = ltcPrivateKey.toPublicKey();
+  var ltcSignatureString = 'H0nSCpviGAHcWlKaWluGSL0mbgTF33SwuDIsaDhFeTkPVae1ryB7fdjGLC513IwhjWnyf78uT2nf6bDV5rQ0eNU=';
+  var ltcSignature = Signature.fromCompact(new Buffer(ltcSignatureString, 'base64'));
 
   it('will error with incorrect message type', function() {
     expect(function() {
@@ -40,12 +51,28 @@ describe('Message', function() {
   var signature2;
   var signature3;
 
-  it('can sign a message', function() {
+  it('can sign a message with BTC private key', function() {
     var message2 = new Message({coinLib: btcLib}, text);
     signature2 = message2._sign(privateKey);
     signature3 = Message({coinLib: btcLib}, text).sign(privateKey);
     should.exist(signature2);
     should.exist(signature3);
+  });
+
+  it('can sign a message with BCH private key', function() {
+    var message2 = new Message({coinLib: bchLib}, text);
+    var signature4 = message2._sign(bchPrivateKey);
+    var signature5 = Message({coinLib: bchLib}, text).sign(bchPrivateKey);
+    should.exist(signature4);
+    should.exist(signature5);
+  });
+
+  it('can sign a message with LTC private key', function() {
+    var message2 = new Message({coinLib: ltcLib}, text);
+    var signature4 = message2._sign(ltcPrivateKey);
+    var signature5 = Message({coinLib: ltcLib}, text).sign(ltcPrivateKey);
+    should.exist(signature4);
+    should.exist(signature5);
   });
 
   it('sign will error with incorrect private key argument', function() {
@@ -55,9 +82,21 @@ describe('Message', function() {
     }).to.throw('First argument should be an instance of PrivateKey');
   });
 
-  it('can verify a message with signature', function() {
+  it('can verify a message with signature BTC', function() {
     var message4 = new Message({coinLib: btcLib}, text);
     var verified = message4._verify(publicKey, signature2);
+    verified.should.equal(true);
+  });
+
+  it('can verify a message with signature BCH', function() {
+    var message4 = new Message({coinLib: bchLib}, text);
+    var verified = message4._verify(bchPublicKey, bchSignature);
+    verified.should.equal(true);
+  });
+
+  it('can verify a message with signature LTC', function() {
+    var message4 = new Message({coinLib: ltcLib}, text);
+    var verified = message4._verify(ltcPublicKey, ltcSignature);
     verified.should.equal(true);
   });
 
@@ -115,6 +154,17 @@ describe('Message', function() {
     verified.should.equal(true);
   });
 
+  it('accepts BTC Address for verification', function() {
+    var verified = Message({coinLib: btcLib}, text)
+      .verify(new btcLib.Address(address), signatureString);
+    verified.should.equal(true);
+  });
+
+  it('can do BCH', function() {
+    var verified = Message({coinLib: btcLib}, text).verify(address, signatureString);
+    verified.should.equal(true);
+  });
+
   describe('#json', function() {
 
     it('roundtrip to-from-to', function() {
@@ -154,12 +204,6 @@ describe('Message', function() {
       message.inspect().should.equal(output);
     });
 
-  });
-
-  it('accepts BTC Address for verification', function() {
-    var verified = Message({coinLib: btcLib}, text)
-      .verify(new btcLib.Address(address), signatureString);
-    verified.should.equal(true);
   });
 
 });
