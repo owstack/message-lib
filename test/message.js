e12ca3655fa7484d8bbf6c9028b6739b2fb35818
ajp8164 <andyphillipson@gmail.com>
2019-02-27 13:33:39 -0500

    Send string into ctor instead of obj, bump version 0.0.2

--- test/message.js
+++ test/message.js
@@ -39,12 +39,12 @@ describe('Message', function() {
 
   it('will error with incorrect message type', function() {
     expect(function() {
-      return new Message({coinLib: btcLib}, new Date());
-    }).to.throw('Second argument should be a string');
+      return new Message(new Date(), 'btc');
+    }).to.throw('First argument should be a string');
   });
 
   it('will instantiate without "new"', function() {
-    var message = Message({coinLib: btcLib}, text);
+    var message = new Message(text, 'btc');
     should.exist(message);
   });
 
@@ -52,90 +52,90 @@ describe('Message', function() {
   var signature3;
 
   it('can sign a message with BTC private key', function() {
-    var message2 = new Message({coinLib: btcLib}, text);
+    var message2 = new Message(text, 'btc');
     signature2 = message2._sign(privateKey);
-    signature3 = Message({coinLib: btcLib}, text).sign(privateKey);
+    signature3 = new Message(text, 'btc').sign(privateKey);
     should.exist(signature2);
     should.exist(signature3);
   });
 
   it('can sign a message with BCH private key', function() {
-    var message2 = new Message({coinLib: bchLib}, text);
+    var message2 = new Message(text, 'bch');
     var signature4 = message2._sign(bchPrivateKey);
-    var signature5 = Message({coinLib: bchLib}, text).sign(bchPrivateKey);
+    var signature5 = new Message(text, 'bch').sign(bchPrivateKey);
     should.exist(signature4);
     should.exist(signature5);
   });
 
   it('can sign a message with LTC private key', function() {
-    var message2 = new Message({coinLib: ltcLib}, text);
+    var message2 = new Message(text, 'ltc');
     var signature4 = message2._sign(ltcPrivateKey);
-    var signature5 = Message({coinLib: ltcLib}, text).sign(ltcPrivateKey);
+    var signature5 = new Message(text, 'ltc').sign(ltcPrivateKey);
     should.exist(signature4);
     should.exist(signature5);
   });
 
   it('sign will error with incorrect private key argument', function() {
     expect(function() {
-      var message3 = new Message({coinLib: btcLib}, text);
+      var message3 = new Message(text, 'btc');
       return message3.sign('not a private key');
     }).to.throw('First argument should be an instance of PrivateKey');
   });
 
   it('can verify a message with signature BTC', function() {
-    var message4 = new Message({coinLib: btcLib}, text);
+    var message4 = new Message(text, 'btc');
     var verified = message4._verify(publicKey, signature2);
     verified.should.equal(true);
   });
 
   it('can verify a message with signature BCH', function() {
-    var message4 = new Message({coinLib: bchLib}, text);
+    var message4 = new Message(text, 'bch');
     var verified = message4._verify(bchPublicKey, bchSignature);
     verified.should.equal(true);
   });
 
   it('can verify a message with signature LTC', function() {
-    var message4 = new Message({coinLib: ltcLib}, text);
+    var message4 = new Message(text, 'ltc');
     var verified = message4._verify(ltcPublicKey, ltcSignature);
     verified.should.equal(true);
   });
 
   it('can verify a message with existing signature', function() {
-    var message5 = new Message({coinLib: btcLib}, text);
+    var message5 = new Message(text, 'btc');
     var verified = message5._verify(publicKey, signature);
     verified.should.equal(true);
   });
 
   it('verify will error with incorrect public key argument', function() {
     expect(function() {
-      var message6 = new Message({coinLib: btcLib}, text);
+      var message6 = new Message(text, 'btc');
       return message6._verify('not a public key', signature);
     }).to.throw('First argument should be an instance of PublicKey');
   });
 
   it('verify will error with incorrect signature argument', function() {
     expect(function() {
-      var message7 = new Message({coinLib: btcLib}, text);
+      var message7 = new Message(text, 'btc');
       return message7._verify(publicKey, 'not a signature');
     }).to.throw('Second argument should be an instance of Signature');
   });
 
   it('verify will correctly identify a bad signature', function() {
-    var message8 = new Message({coinLib: btcLib}, text);
+    var message8 = new Message(text, 'btc');
     var verified = message8._verify(publicKey, badSignature);
     should.exist(message8.error);
     verified.should.equal(false);
   });
 
   it('can verify a message with address and generated signature string', function() {
-    var message9 = new Message({coinLib: btcLib}, text);
+    var message9 = new Message(text, 'btc');
     var verified = message9.verify(address, signature3);
     should.not.exist(message9.error);
     verified.should.equal(true);
   });
 
   it('will not verify with address mismatch', function() {
-    var message10 = new Message({coinLib: btcLib}, text);
+    var message10 = new Message(text, 'btc');
     var verified = message10.verify(badAddress, signatureString);
     should.exist(message10.error);
     verified.should.equal(false);
@@ -143,39 +143,39 @@ describe('Message', function() {
 
   it('will verify with an uncompressed pubkey', function() {
     var privateKey = new keyLib.PrivateKey('5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss', 'btc');
-    var message = new Message({coinLib: btcLib}, 'This is an example of a signed message.');
+    var message = new Message('This is an example of a signed message.', 'btc');
     var signature = message.sign(privateKey);
     var verified = message.verify(btcLib.Address.fromPrivateKey(privateKey), signature);
     verified.should.equal(true);
   });
 
   it('can chain methods', function() {
-    var verified = Message({coinLib: btcLib}, text).verify(address, signatureString);
+    var verified = new Message(text, 'btc').verify(address, signatureString);
     verified.should.equal(true);
   });
 
   it('accepts BTC Address for verification', function() {
-    var verified = Message({coinLib: btcLib}, text)
+    var verified = new Message(text, 'btc')
       .verify(new btcLib.Address(address), signatureString);
     verified.should.equal(true);
   });
 
   it('can do BCH', function() {
-    var verified = Message({coinLib: btcLib}, text).verify(address, signatureString);
+    var verified = new Message(text, 'btc').verify(address, signatureString);
     verified.should.equal(true);
   });
 
   describe('#json', function() {
 
     it('roundtrip to-from-to', function() {
-      var json = new Message({coinLib: btcLib}, text).toJSON();
-      var message = Message.fromJSON({coinLib: btcLib}, json);
+      var json = new Message(text, 'btc').toJSON();
+      var message = new Message.fromJSON(json, 'btc');
       message.toString().should.equal(text);
     });
 
     it('checks that the string parameter is valid JSON', function() {
       expect(function() {
-        return Message.fromJSON({coinLib: btcLib}, '¹');
+        return Message.fromJSON('¹', 'btc');
       }).to.throw();
     });
 
@@ -184,13 +184,13 @@ describe('Message', function() {
   describe('#toString', function() {
 
     it('message string', function() {
-      var message = new Message({coinLib: btcLib}, text);
+      var message = new Message(text, 'btc');
       message.toString().should.equal(text);
     });
 
     it('roundtrip to-from-to', function() {
-      var str = new Message({coinLib: btcLib}, text).toString();
-      var message = Message.fromString({coinLib: btcLib}, str);
+      var str = new Message(text, 'btc').toString();
+      var message = new Message.fromString(str, 'btc');
       message.toString().should.equal(text);
     });
 
@@ -199,7 +199,7 @@ describe('Message', function() {
   describe('#inspect', function() {
 
     it('should output formatted output correctly', function() {
-      var message = new Message({coinLib: btcLib}, text);
+      var message = new Message(text, 'btc');
       var output = '<Message: '+text+'>';
       message.inspect().should.equal(output);
     });
